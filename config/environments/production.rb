b13171e761b2ceb8f6ef8542bdbe54a4ed02e029
Austin Wilson <austin@newcoastventures.com>
2020-12-23 15:09:13 -0500

    attempt 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://www.dirty-santa.app/cable'
-  config.action_cable.allowed_request_origins = [ 'https://www.dirty-santa.app', /http:\/\/dirty-santa.app*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://www.dirty-santa.app', /http:\/\/www.dirty-santa.app*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
