b2d8e4749c8497be59725ed3c2394f9b1950d7d4
Austin Wilson <austin@newcoastventures.com>
2020-12-23 14:48:39 -0500

    update to use https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://dirty-santa.app/cable'
-  config.action_cable.allowed_request_origins = [ 'http://dirty-santa.app', /http:\/\/dirty-santa.app*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://dirty-santa.app', /https:\/\/dirty-santa.app*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
